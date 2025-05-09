ef83a7a806e9eaa9dc24f9a3db956cbe99187fd3
Atif-Muhammad <mohatif02@gmail.com>
2025-05-09 18:27:29 +0500

    eee

--- Frontend/src/components/Ads.jsx
+++ Frontend/src/components/Ads.jsx
@@ -1,43 +1,53 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Ads = () => {
   const adRef = useRef(null);
+  const [scriptLoaded, setScriptLoaded] = useState(false);
 
   useEffect(() => {
-    const pushAds = () => {
-      try {
-        if (window.adsbygoogle && adRef.current) {
-          window.adsbygoogle.push({});
-        }
-      } catch (e) {
-        console.error("Adsense error:", e);
+    const loadAdsScript = () => {
+      if (scriptLoaded || window.adsbygoogle) {
+        setScriptLoaded(true);
+        return;
       }
-    };
 
-    // Check if the script is already loaded
-    if (!document.querySelector('script[src*="adsbygoogle.js"]')) {
       const script = document.createElement("script");
       script.src =
         "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2917906237665869";
       script.async = true;
       script.crossOrigin = "anonymous";
-      script.onload = pushAds;
+      script.onload = () => setScriptLoaded(true);
       document.body.appendChild(script);
-    } else {
-      pushAds();
+    };
+
+    loadAdsScript();
+  }, [scriptLoaded]);
+
+  useEffect(() => {
+    if (scriptLoaded && adRef.current) {
+      try {
+        window.adsbygoogle.push({});
+      } catch (e) {
+        console.error("Adsense error:", e);
+      }
     }
-  }, []);
+  }, [scriptLoaded]);
 
   return (
-    <ins
-      className="adsbygoogle"
-    //   style={{ display: "block" }}
-      data-ad-client="ca-pub-2917906237665869"
-      data-ad-slot="2932306120"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-      ref={adRef}
-    />
+    <div className="w-full h-full overflow-hidden flex items-center justify-center bg-red-500">
+      <ins
+        className="adsbygoogle "
+        style={{
+          display: "block",
+          width: "120px",
+        }}
+        data-ad-client="ca-pub-2917906237665869"
+        data-ad-slot="2932306120"
+        data-ad-format="auto"
+        data-full-width-responsive="false"
+        ref={adRef}
+      />
+    </div>
   );
 };
 
