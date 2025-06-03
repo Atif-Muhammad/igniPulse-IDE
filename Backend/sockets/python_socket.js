48f2f14e9f505bd5523a0d7205f7a3d45072eef5
Atif-Muhammad <mohatif02@gmail.com>
2025-06-03 18:50:51 +0500

    dsad

--- Backend/sockets/python_socket.js
+++ Backend/sockets/python_socket.js
@@ -60,7 +60,7 @@ plt.show = safe_show
         code,
       ];
 
-      const pyProcess = spawn("/usr/bin/docker", dockerArgs);
+      const pyProcess = spawn("docker", dockerArgs);
       // const pyProcess = require("child_process").spawn("docker", dockerArgs);
 
       let errorOutput = "";
