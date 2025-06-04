58aa1fbfa8b20091bb7bd4bff5fb9dbfb65d9a32
Atif-Muhammad <mohatif02@gmail.com>
2025-06-04 11:52:47 +0500

    dsa

--- Backend/sockets/python_socket.js
+++ Backend/sockets/python_socket.js
@@ -119,7 +119,7 @@ plt.show = safe_show
           // const buffer = fs.readFileSync(fullOutputPath);
           // const base64Image = buffer.toString("base64");
           // console.log("sending graph:",base64Image);
-          const localPath = `/app/temps/${outputFile}`;
+          const localPath = `/temps/${outputFile}`;
           const base64Image = fs.readFileSync(localPath).toString("base64");
           socket.emit("graphOutput", `data:image/png;base64,${base64Image}`);
           // fs.unlinkSync(fullOutputPath);
